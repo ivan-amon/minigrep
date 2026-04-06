2265a00c86912edab16985c78ba95b39876d97db
Ivan <iab050811@gmail.com>
2026-04-06 12:59:59 +0200

    feat: accepting command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
